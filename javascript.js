ef12325975b092649425810db7fccd256599d816
momo427 <ferguson.tyra@gmail.com>
2021-02-10 14:30:21 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,27 @@
+alert("Hello");
+
+
+    function growFunction(){
+
+        document.getElementById("box").style.height = "250px";
+
+    };
+
+    function blueFunction(){
+
+        document.getElementById("box").style.backgroundColor = "blue";
+
+    };
+
+    function fadeFunction(){
+
+        document.getElementById("box").style.opacity = .2;
+
+    };
+
+    function resetFunction(){
+
+        document.getElementById("box").style.height = "150px";
+        document.getElementById("box").style.backgroundColor = "orange";
+        document.getElementById("box").style.opacity = 1;
+    };
